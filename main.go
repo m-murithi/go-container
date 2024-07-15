02c53ef89caa1d42f6bcefccce27c961120afc0c
marvol0 <murithimarvin09@gmail.com>
2024-07-15 16:25:09 +0300

    feat: set up the container environment using namespaces

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "os/exec"
+    "syscall"
+)
+
+func main() {
+    cmd := exec.Command("/proc/self/exe", "child")
+    cmd.SysProcAttr = &syscall.SysProcAttr{
+        Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC,
+    }
+
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+
+    if err := cmd.Run(); err != nil {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
+}
+
+func child() {
+    fmt.Println("Running inside the container")
+    cmd := exec.Command("/bin/sh")
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    if err := cmd.Run(); err != nil {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
+}
+
+func init() {
+    if len(os.Args) > 1 && os.Args[1] == "child" {
+        child()
+        os.Exit(0)
+    }
+}
